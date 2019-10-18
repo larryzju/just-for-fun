6c305333b81f4130e4e8e2a99da29c8b6ce526e7
Larry Zhao <larry.zhao01@sap.com>
2019-10-18 01:03:14 -0400

    draw border

--- tetris/main.go
+++ tetris/main.go
@@ -17,8 +17,12 @@ const (
 
 const (
 	Width  = 12
-	Height = 25
+	Height = 12
 )
+
+type Point struct {
+	x, y int
+}
 
 func cleanScreen() {
 	fmt.Printf("%s[s", escape)
@@ -28,8 +32,33 @@ func moveCursor(line, col int) {
 	fmt.Printf("%s[%d;%dH", escape, line, col)
 }
 
+func drawBoard(topLeft Point, width, height int) {
+	moveCursor(topLeft.x, topLeft.y)
+	fmt.Printf("%c", BorderTopLeft)
+	for i := 0; i < width; i++ {
+		fmt.Printf("%c", BorderHorization)
+	}
+	fmt.Printf("%c", BorderTopRight)
+
+	for i := 1; i < height; i++ {
+		moveCursor(topLeft.y+i, topLeft.x)
+		fmt.Printf("%c", BorderVertical)
+		moveCursor(topLeft.y+i, topLeft.x+width+1)
+		fmt.Printf("%c", BorderVertical)
+	}
+
+	moveCursor(topLeft.y+height, topLeft.x)
+	fmt.Printf("%c", BorderBottomLeft)
+	for i := 0; i < width; i++ {
+		fmt.Printf("%c", BorderHorization)
+	}
+	fmt.Printf("%c", BorderBottomRight)
+}
+
 func main() {
 	cleanScreen()
-	moveCursor(10, 10)
-	fmt.Printf("HAHA")
+	drawBoard(Point{10, 10}, Width, Height)
+	moveCursor(12, 12)
+	fmt.Printf("hahahaa")
+	moveCursor(80, 0)
 }
