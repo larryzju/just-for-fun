6a5a1c3ee41565b990b2010247f5b5b913e60227
Larry Zhao <larry.zhao01@sap.com>
2019-10-18 03:53:34 -0400

    define tetris shapes

--- tetris/main.go
+++ tetris/main.go
@@ -21,8 +21,33 @@ const (
 	Height = 12
 )
 
+var (
+	ShapeL = Shape{".X..X..XX", 3}
+	ShapeF = Shape{".XX.X..X.", 3}
+	ShapeT = Shape{"...XXX.X.", 3}
+	ShapeZ = Shape{"XX..XX...", 3}
+	Shape5 = Shape{".XX.X.XX.", 3}
+	ShapeO = Shape{"XXXX", 2}
+	ShapeI = Shape{"....XXXX.........", 4}
+)
+
+type Shape struct {
+	data string
+	n    int
+}
+
+func (s Shape) Rotate() Shape {
+	bytes := make([]byte, len(s.data))
+	for i := 0; i < s.n*s.n; i++ {
+		r, c := i/s.n, i%s.n
+		t := c*s.n + s.n - r - 1
+		bytes[t] = s.data[i]
+	}
+	return Shape{string(bytes), s.n}
+}
+
 type Point struct {
-	x, y int
+	y, x int
 }
 
 func cleanScreen() {
@@ -40,21 +65,21 @@ type Game struct {
 }
 
 func (g *Game) drawBoard(topLeft Point, width, height int) {
-	moveCursor(topLeft.x, topLeft.y)
+	moveCursor(topLeft.x-1, topLeft.y-1)
 	fmt.Printf("%c", BorderTopLeft)
 	for i := 0; i < width; i++ {
 		fmt.Printf("%c", BorderHorization)
 	}
 	fmt.Printf("%c", BorderTopRight)
 
-	for i := 1; i < height; i++ {
-		moveCursor(topLeft.y+i, topLeft.x)
+	for i := 0; i < height; i++ {
+		moveCursor(topLeft.y+i, topLeft.x-1)
 		fmt.Printf("%c", BorderVertical)
-		moveCursor(topLeft.y+i, topLeft.x+width+1)
+		moveCursor(topLeft.y+i, topLeft.x+width)
 		fmt.Printf("%c", BorderVertical)
 	}
 
-	moveCursor(topLeft.y+height, topLeft.x)
+	moveCursor(topLeft.y+height, topLeft.x-1)
 	fmt.Printf("%c", BorderBottomLeft)
 	for i := 0; i < width; i++ {
 		fmt.Printf("%c", BorderHorization)
@@ -70,24 +95,42 @@ func (g *Game) flushScore() {
 	fmt.Printf("Level:   %2d", g.Level)
 }
 
+func (g *Game) flushShape(o Point, s Shape, clear bool) {
+	for i := 0; i < s.n*s.n; i++ {
+		r, c := i/s.n, i%s.n
+		if s.data[i] != '.' {
+			moveCursor(g.Origin.y+o.y+r, g.Origin.x+o.x+c)
+			char := s.data[i]
+			if clear {
+				char = ' '
+			}
+			fmt.Printf("%c", char)
+		}
+	}
+}
+
 func (g *Game) init() {
 	cleanScreen()
 	g.drawBoard(Point{10, 10}, Width, Height)
 	g.flushScore()
 	moveCursor(12, 12)
-	fmt.Printf("hahahaa")
 }
 
 func main() {
 	g := Game{Origin: Point{10, 10}}
 	g.init()
 	go func() {
+		s := ShapeL
 		for {
 			g.Score += 1
-			time.Sleep(time.Millisecond * 200)
+			g.flushShape(Point{0, 0}, s, false)
+			time.Sleep(time.Millisecond * 500)
+			g.flushShape(Point{0, 0}, s, true)
+			s = s.Rotate()
 			g.flushScore()
 		}
 	}()
+
 	time.Sleep(10 * time.Second)
 	moveCursor(80, 0)
 }
--- tetris/roadmap.md
+++ tetris/roadmap.md
@@ -1,5 +1,5 @@
 * [x] move cursor and print
 * [x] print the board
 * [x] add score board
+* [x] define 7 types of tetris which can be rotated
 * [ ] object moves down in ticks
-* [ ] define 7 types of tetris which can be rotated
