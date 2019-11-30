0657f6fe9198e2e526e81099956d8235c910c71b
Zhao WenBin <larryzju@163.com>
2019-11-30 10:00:43 +0800

    abstract Input and Display interface

--- tetris/main.go
+++ tetris/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"syscall"
 	"time"
 )
+
+// Input abstract the keyboard input devices
+type Input interface {
+	Init()
+}
+
+// Display abstract the UI
+type Display interface {
+	Init()
+	DrawTetris(t *Tetris, clear bool)
+	UpdateScore(score, level int)
+}
 
 const escape = "\x1b"
 
@@ -29,22 +39,24 @@ type Block struct {
 
 var defaultBlock = &Block{}
 
-var (
-	ShapeL = Shape{".X..X..XX", 3, defaultBlock}
-	ShapeF = Shape{".XX.X..X.", 3, defaultBlock}
-	ShapeT = Shape{"...XXX.X.", 3, defaultBlock}
-	ShapeZ = Shape{"XX..XX...", 3, defaultBlock}
-	Shape5 = Shape{".XX.X.XX.", 3, defaultBlock}
-	ShapeO = Shape{"XXXX", 2, defaultBlock}
-	ShapeI = Shape{"....XXXX.........", 4, defaultBlock}
-)
+var shapes = []Shape{
+	Shape{".X..X..XX", 3, defaultBlock}, // L
+	Shape{".XXkX..X.", 3, defaultBlock}, // F
+	Shape{"...XXX.X.", 3, defaultBlock}, // T
+	Shape{"XX..XX...", 3, defaultBlock}, // Z
+	Shape{".XX.X.XX.", 3, defaultBlock}, // 5
+	Shape{"XXXX", 2, defaultBlock}, // o
+	Shape{"....XXXX.........", 4, defaultBlock}, // I
+}
 
+// Shape of the tetris
 type Shape struct {
 	data  string
 	n     int
 	block *Block
 }
 
+// Rotate shape clockwise
 func (s Shape) Rotate() Shape {
 	bytes := make([]byte, len(s.data))
 	for i := 0; i < s.n*s.n; i++ {
@@ -55,94 +67,75 @@ func (s Shape) Rotate() Shape {
 	return Shape{string(bytes), s.n, s.block}
 }
 
+// Point for coordinator
 type Point struct {
 	y, x int
-}
-
-func cleanScreen() {
-	fmt.Printf("%s[s", escape)
-}
-
-func hideCursor() {
-	// fmt.Printf("\e[?25l")
 }
 
 func moveCursor(line, col int) {
 	fmt.Printf("%s[%d;%dH", escape, line, col)
 }
 
+// Tetris is a shape in specify position
+type Tetris struct {
+	shape Shape
+	pos   Point
+}
+
+// Move tetris x blocks right and y blocks down
+func(t *Tetris) Move(y, x int) *Tetris {
+	return &Tetris {
+		pos: Point{t.pos.y + y, t.pos.x + x},
+		shape: t.shape,
+	}
+}
+
+// Rotate tetris clockwise
+func(t *Tetris) Rotate() *Tetris {
+	return &Tetris {
+		pos: t.pos,
+		shape: t.shape.Rotate(),
+	}
+}
+
 type Game struct {
+	Input Input
+	Display Display
 	Score  int
 	Level  int
-	Origin Point
-	shape  Shape
-	pos    Point
+	prev, cur *Tetris
 	board  []*Block
 }
 
-func (g *Game) drawBoard(topLeft Point, width, height int) {
-	moveCursor(topLeft.x-1, topLeft.y-1)
-	fmt.Printf("%c", BorderTopLeft)
-	for i := 0; i < width; i++ {
-		fmt.Printf("%c", BorderHorization)
-	}
-	fmt.Printf("%c", BorderTopRight)
 
-	for i := 0; i < height; i++ {
-		moveCursor(topLeft.y+i, topLeft.x-1)
-		fmt.Printf("%c", BorderVertical)
-		moveCursor(topLeft.y+i, topLeft.x+width)
-		fmt.Printf("%c", BorderVertical)
+
+func (g *Game) FlushTetris() {
+	if g.prev != nil {
+		g.Display.DrawTetris(g.prev, true)
 	}
 
-	moveCursor(topLeft.y+height, topLeft.x-1)
-	fmt.Printf("%c", BorderBottomLeft)
-	for i := 0; i < width; i++ {
-		fmt.Printf("%c", BorderHorization)
-	}
-	fmt.Printf("%c", BorderBottomRight)
+	g.Display.DrawTetris(g.cur, false)
 }
 
-func (g *Game) flushScore() {
-	moveCursor(g.Origin.y+1, g.Origin.x+Width+3)
-	fmt.Printf("Score:%05d", g.Score)
 
-	moveCursor(g.Origin.y+2, g.Origin.x+Width+3)
-	fmt.Printf("Level:   %2d", g.Level)
+// NewTetris generate tetris at the top
+func (g *Game) NewTetris(seed int) *Tetris{
+	shape := shapes[seed%(len(shapes))]
+	pos := Point{0, Width/2 - shape.n/2}
+	return &Tetris{shape: shape, pos: pos}
 }
 
-func (g *Game) flushShape(o Point, s Shape, clear bool) {
-	for i := 0; i < s.n*s.n; i++ {
-		r, c := i/s.n, i%s.n
-		if s.data[i] != '.' {
-			moveCursor(g.Origin.y+o.y+r, g.Origin.x+o.x+c)
-			char := s.data[i]
-			if clear {
-				char = ' '
-			}
-			fmt.Printf("%c", char)
-		}
-	}
-}
-
-func (g *Game) newShape(s Shape) {
-	g.shape = s
-	g.pos = Point{0, Width/2 - s.n/2}
-	g.flushShape(g.pos, s, false)
-	g.board = make([]*Block, Width*Height)
-}
-
-func (g *Game) Next(delta Point) {
-	nextPos := Point{g.pos.y + delta.y, g.pos.x + delta.x}
-	if g.isCollision(nextPos, g.shape) {
+func (g *Game) Transform(next *Tetris) {
+	if g.isCollision(next) {
 		return
 	}
-	g.flushShape(g.pos, g.shape, true)
-	g.pos = nextPos
-	g.flushShape(g.pos, g.shape, false)
+
+	g.cur, g.prev = next, g.cur
+	g.FlushTetris()
 }
 
-func (g *Game) isCollision(pos Point, s Shape) bool {
+func (g *Game) isCollision(t *Tetris) bool {
+	pos, s := t.pos, t.shape
 	for i := 0; i < s.n*s.n; i++ {
 		if s.data[i] == '.' {
 			continue
@@ -206,51 +199,46 @@ func (g *Game) Listen() {
 		case key := <-input:
 			switch key {
 			case Left:
-				g.Next(Point{0, -1})
+				g.Transform(g.cur.Move(0,-1))
 			case Right:
-				g.Next(Point{0, 1})
+				g.Transform(g.cur.Move(0,1))
 			case Down:
-				g.Next(Point{1, 0})
+				g.Transform(g.cur.Move(1,0))
 			case TurnRight:
-				newShape := g.shape.Rotate()
-				if !g.isCollision(g.pos, newShape) {
-					g.flushShape(g.pos, g.shape, true)
-					g.shape = newShape
-					g.flushShape(g.pos, g.shape, false)
-				}
+				g.Transform(g.cur.Rotate())
 			}
 		case <-tick:
-			if !g.isCollision(Point{g.pos.y + 1, g.pos.x}, g.shape) {
-				g.Next(Point{1, 0})
-			}
-			g.flushScore()
+			g.Transform(g.cur.Move(1,0))
 		}
 	}
 }
 
-func (g *Game) init() {
-	mode, err := TCGetLocalMode(os.Stdin.Fd())
-	if err != nil {
-		log.Fatal(err)
-	}
+func (g *Game) Init() {
+	g.Input.Init()
+	g.Display.Init()
 
-	mode &= ^uint64(syscall.ECHO | syscall.ICANON)
-	log.Printf("%08X\n", mode)
-	if err := TCSetLocalMode(os.Stdin.Fd(), mode); err != nil {
-		log.Fatal(err)
-	}
+	// init board
+	g.board = make([]*Block, Width*Height)
 
-	cleanScreen()
-	hideCursor()
-	g.drawBoard(Point{10, 10}, Width, Height)
-	g.flushScore()
-	moveCursor(12, 12)
+	// init tetris
+	// TODO random
+	g.cur = g.NewTetris(0)
+	g.FlushTetris()
+
+	// show score board
+	g.Display.UpdateScore(g.Score, g.Level)
 }
 
 func main() {
-	g := Game{Origin: Point{10, 10}}
-	g.init()
-	g.newShape(ShapeT)
+	g := Game{
+		Input: &TTYInput{}, 
+		Display: &TTYDisplay{
+			Origin: Point{10,10},
+			Width: Width,
+			Height: Height,
+		},
+	}
+
+	g.Init()
 	g.Listen()
-	moveCursor(80, 0)
 }
--- tetris/tty.go
+++ tetris/tty.go
@@ -5,7 +5,12 @@ package main
 // #include <termios.h>
 // #include <stdio.h>
 import "C"
-import "fmt"
+import (
+	"os"
+	"fmt"
+	"log"
+	"syscall"
+)
 
 // TCGetLocalMode get tty local mode setting
 func TCGetLocalMode(fd uintptr) (uint64, error) {
@@ -29,4 +34,89 @@ func TCSetLocalMode(fd uintptr, mode uint64) error {
 	}
 
 	return nil
+}
+
+// TTYInput wraps input of the tty
+type TTYInput struct{}
+
+// Init the console
+func(tty *TTYInput) Init() {
+	mode, err := TCGetLocalMode(os.Stdin.Fd())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mode &= ^uint64(syscall.ECHO | syscall.ICANON)
+	log.Printf("%08X\n", mode)
+	if err := TCSetLocalMode(os.Stdin.Fd(), mode); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// TTYDisplay show tetris on console
+type TTYDisplay struct {
+	Origin Point
+	Width int
+	Height int
+}
+
+// Init the display
+func(tty *TTYDisplay) Init() {
+	cleanScreen()
+	hideCursor()
+	drawBoard(tty.Origin, tty.Width, tty.Height)
+}
+
+func cleanScreen() {
+	fmt.Printf("%s[s", escape)
+}
+
+func hideCursor() {
+	// fmt.Printf("\e[?25l")
+}
+
+func drawBoard(topLeft Point, width, height int) {
+	moveCursor(topLeft.x-1, topLeft.y-1)
+	fmt.Printf("%c", BorderTopLeft)
+	for i := 0; i < width; i++ {
+		fmt.Printf("%c", BorderHorization)
+	}
+	fmt.Printf("%c", BorderTopRight)
+
+	for i := 0; i < height; i++ {
+		moveCursor(topLeft.y+i, topLeft.x-1)
+		fmt.Printf("%c", BorderVertical)
+		moveCursor(topLeft.y+i, topLeft.x+width)
+		fmt.Printf("%c", BorderVertical)
+	}
+
+	moveCursor(topLeft.y+height, topLeft.x-1)
+	fmt.Printf("%c", BorderBottomLeft)
+	for i := 0; i < width; i++ {
+		fmt.Printf("%c", BorderHorization)
+	}
+	fmt.Printf("%c", BorderBottomRight)
+}
+
+func (d *TTYDisplay) DrawTetris(t *Tetris, clear bool) {
+	o, s := t.pos, t.shape
+	for i := 0; i < s.n*s.n; i++ {
+		r, c := i/s.n, i%s.n
+		if s.data[i] != '.' {
+			moveCursor(d.Origin.y+o.y+r, d.Origin.x+o.x+c)
+			char := s.data[i]
+			if clear {
+				char = ' '
+			}
+			fmt.Printf("%c", char)
+		}
+	}
+}
+
+func (d *TTYDisplay) UpdateScore(score, level int) {
+	moveCursor(d.Origin.y+1, d.Origin.x+d.Width+3)
+	fmt.Printf("Score:%05d", score)
+
+	moveCursor(d.Origin.y+2, d.Origin.x+d.Width+3)
+	fmt.Printf("Level:   %2d", level)
 }
